498cb47ecc0c3b525a2e33e08f969ffbbd0323fd
cubitos_magui <magui081191@gmail.com>
2022-04-29 10:53:43 -0400

    Terminado el crud

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { ViewGenerosComponent } from './components/genero/view-generos/view-generos.component';
+import {HttpClientModule} from '@angular/common/http';
+import {ApiGeneroService} from 'src/app/services/genero/api-genero.service';
+import {ApiPeliculaService} from "./services/pelicula/api-pelicula.service";
+import { ViewPeliculaComponent } from './components/pelicula/view-pelicula/view-pelicula.component';
+import { AddUpdateGeneroComponent } from './components/genero/add-update-genero/add-update-genero.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewGenerosComponent,
+    ViewPeliculaComponent,
+    AddUpdateGeneroComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [ApiGeneroService, ApiPeliculaService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
